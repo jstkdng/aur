4bedb6442aaa1a7ed06b56833b4174d9e5adbcfe
JustKidding <jk@vin.ovh>
2022-02-04 21:48:39 -0500

    add python script

---
+++ .github/workflows/mirror/gen-service.py
@@ -0,0 +1,57 @@
+import os
+from srcinfo.parse import parse_srcinfo
+from urllib.parse import urlparse
+from jinja2 import Environment, FileSystemLoader, BaseLoader
+
+TEMPLATE_STR = """
+<services>
+{%- for source in sources %}
+    <service name="download_url">
+        <param name="protocol">{{ source.protocol }}</param>
+        <param name="host">{{ source.host }}</param>
+        <param name="path">{{ source.path }}</param>
+        <param name="filename">{{ source.filename }}</param>
+    </service>
+{%- endfor %}
+</services>
+
+"""
+
+
+def main():
+    # read .SRCINFO from current directory
+    with open(".SRCINFO", "r") as f:
+        (srcinfo, _) = parse_srcinfo(f.read())
+        sources = []
+        for source in srcinfo["source"]:
+            parsed_url = urlparse(source)
+            src_obj = {
+                "protocol": "",
+                "host": "",
+                "path": "",
+                "filename": ""
+            }
+
+            if not parsed_url.netloc:
+                if parsed_url.scheme:
+                    src_obj["filename"] = parsed_url.scheme
+                    parsed_url = urlparse(parsed_url.path[1:])
+                else:
+                    continue
+
+            src_obj["protocol"] = parsed_url.scheme
+            src_obj["host"] = parsed_url.netloc
+            src_obj["path"] = parsed_url.path
+            if not src_obj["filename"]:
+                src_obj["filename"] = parsed_url.path.split('/')[-1]
+
+            sources.append(src_obj)
+    template = Environment(loader=BaseLoader()).from_string(TEMPLATE_STR)
+    output = template.render(sources=sources)
+
+    with open("_service", "w") as f:
+        f.write(output)
+
+
+if __name__ == "__main__":
+    main()
